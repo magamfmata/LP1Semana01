acfa5f7a71a39becdddbca1e2ae855b313b87dae
Magali <magamayrmata@gmail.com>
2025-02-21 01:30:42 +0000

    Strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text; 
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello LP1!");
+
+            string string1 = "texto\t e '\u263d'";
+            Console.WriteLine(string1);
+
+            string string2 = "texto2\t e '\u2646'";
+            Console.WriteLine(string2);
+        }
+    }
+}
