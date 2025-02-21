b3301c72625c29c4f29ca9f55f6243d9cb90d098
Magali <magamayrmata@gmail.com>
2025-02-21 00:32:02 +0000

    Comment

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -4,6 +4,10 @@ namespace MeuPrograma
 {
     public class Program
     {
+        /// <summary>
+        /// start of code
+        /// </summary>
+        /// <param name="args">comment</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
