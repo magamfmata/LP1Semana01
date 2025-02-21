3dc1d807dc359895de5e28f67bd2eb03c0b0383b
Magali <magamayrmata@gmail.com>
2025-02-21 00:51:36 +0000

    simbolos

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-
+using System.Text; 
 namespace VariousTypes
 {
     public class Program
     {
         private static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("Hello LP1!");
             int variavel1 = 6;
             uint variavel2 = 6U; 
@@ -16,6 +17,12 @@ namespace VariousTypes
             Console.WriteLine(variavel2);
             Console.WriteLine(variavel3);
             Console.WriteLine(variavel4);
+
+            char estrela = '\u269D';
+            Console.WriteLine(estrela);
+
+            char lua = '\u263D';
+            Console.WriteLine(lua);
 
         }
     }
