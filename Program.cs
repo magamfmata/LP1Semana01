deca51ad450cd18af6f098422d24071fa582b0de
Magali <magamayrmata@gmail.com>
2025-02-21 00:03:38 +0000

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LP1Semana01
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
